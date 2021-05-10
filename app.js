75092f47a846e85467bb3e3b6721a7f0137a7e14
Morfo <morfo.frankopoulou@cognitivplus.com>
2021-05-10 14:31:09 +0300

    Channel trade with subchannels 'btn-eur' and 'eth-eur'

--- app.js
+++ app.js
@@ -3,34 +3,22 @@ import WebSocket from 'ws';
 import { createServer } from 'http';
 import { authorize } from './services/userService.js';
 import init from './scripts/init.js';
+import handleMessage from './ws/channels.js';
 
 const app = express();
 const port = 8080;
 
 init();
 
-
 const server = createServer(app);
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws, request) => {
-  console.log(request.headers);
   if (authorize(request.headers)) {
-    console.log('connected');
     ws.on('message', (message) => {
-      console.log('received: %s', message);
-      const msg = JSON.parse(message);
-      if (msg.type === 'ping') {
-        ws.send('pong');
-        console.log('ping recieved and pong sent');
-      }
+      handleMessage(ws, message);
     });
-    ws.on('ping', () => {
-      console.log('ping received');
-      ws.send(JSON.stringify({ "type": 'pong' }));
-    });
-
-    ws.send('msg from server');
+    ws.send('Connected');
   }
 });
 
---
+++ ws/channels.js
@@ -0,0 +1,83 @@
+const handleMessage = (ws, message) => {
+  const msg = JSON.parse(message);
+  console.log(msg);
+  switch(msg.type) {
+    case 'ping':
+      ping(ws);
+      break;
+    case 'subscribe':
+      subscribe(ws, msg);
+      break;
+    case 'unsubscribe':
+      unsubscribe(ws, msg);
+      break;
+      case 'session':
+        break;
+    default:
+      console.log('Invalid request');
+  }
+};
+
+const ping = (ws) => {
+  console.log()
+  ws.send('{ "type": "pong" }');
+};
+
+const subscribe = (ws, message) => {
+  if (message.chan_name === 'trade') {
+    return trade(ws, message.subchan_name);
+  }
+};
+
+const unsubscribe = (ws, message) => {
+  console.log(`Unsubscribing: ${message}`);
+  if (message.chan_name === 'trade') {
+    if (message.subchan_name === 'btc-eur' && ws.tradeBtcEur) {
+      clearInterval(ws.tradeBtcEur);
+      console.log('Unsubscribed from Trade channel Btc-Eur subchannel');
+    }
+    else if (message.subchan_name === 'eth-eur' && ws.tradeEthEur) {
+      clearInterval(ws.tradeEthEur);
+      console.log('Unsubscribed from Trade channel Eth-Eur subchannel');
+    }
+  }
+};
+
+const trade = (ws, subChannel) => {
+  switch (subChannel) {
+    case 'btc-eur':
+      tradeBtcEur(ws);
+      break;
+    case 'eth-eur':
+      tradeEthEur(ws);
+      break;
+    default:
+      console.log('Sub-channel does not exist');
+      break;
+  }
+};
+
+const tradeBtcEur = (ws) => {
+  const interval = setInterval(() => {
+    console.log('sending Bitcoin to Euro Trades');
+    ws.send(getTradeData('btc-eur'));
+  }, 10000);
+  ws.tradeBtcEur = interval;
+};
+
+const tradeEthEur = (ws) => {
+  const interval = setInterval(() => {
+    console.log('sending Ethereum to Euro Trades');
+    ws.send(getTradeData('eth-eur'));
+  }, 10000);
+  ws.tradeEthEur = interval;
+};
+
+const getTradeData = (subChannel) => {
+  return `{"chan_name": "trade", "subchan_name": "${subChannel}", "type":"data","data":[{"price":"8267.7","side":"sell","size":"0","market":"${subChannel}"},{"price":"8267.6","side":"sell","size":"3.02385214","market":"${subChannel}"}]}`
+};
+
+export default handleMessage;
+
+
+
